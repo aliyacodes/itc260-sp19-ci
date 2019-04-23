cde24e3903adb26a5df1b6b5a60a1888db0ac7fb
aliyacodes <aasken01@seattlecentral.edu>
2019-04-23 12:04:26 -0700

    Adds Create method and views
    Now we can add news items.

--- sp19/application/views/news/create.php
+++ sp19/application/views/news/create.php
@@ -1,11 +1,17 @@
 <?php
 //application/views/news/create.php
+
+$this->load->view($this->config->item('theme') . 'header');
+
 ?>
+
 <h2><?php echo $title; ?></h2>
 
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('news/create'); ?>
+
+    
 
     <label for="title">Title</label>
     <input type="input" name="title" /><br />
@@ -16,3 +22,10 @@
     <input type="submit" name="submit" value="Create news item" />
 
 </form>
+
+<?php
+
+$this->load->view($this->config->item('theme') . 'footer');
+
+?>
+
